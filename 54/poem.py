949c3fc30b67b8b25fc553128fe79764c4979566
Peter Ziersch <peter.ziersch@ecocentric.energy>
2024-04-16 10:06:47 +0800

    pybites challenge 54

--- 54/poem.py
+++ 54/poem.py
@@ -1,5 +1,37 @@
 INDENTS = 4
+# part of Remember, by Christina Rosetti
+rosetti_unformatted = """
+                      Remember me when I am gone away,
+                      Gone far away into the silent land;
+                      When you can no more hold me by the hand,
+
+                      Nor I half turn to go yet turning stay.
+
+                      Remember me when no more day by day
+                      You tell me of our future that you planned:
+                      Only remember me; you understand
+                      """
 
 
 def print_hanging_indents(poem):
-    pass
+    indent_flag = True
+    for line in poem.split('\n'):
+        trimmed = line.strip()
+        # print(f"{len(trimmed)}", end=' - ')
+        if indent_flag:
+            skip_blank_lines(trimmed)
+            if len(trimmed) != 0:
+                indent_flag = False
+        else:
+            if len(trimmed) == 0:
+                indent_flag = True
+                continue
+            skip_blank_lines(" " * INDENTS + f"{trimmed}")
+
+
+def skip_blank_lines(line):
+    if line:
+        print(f"{line}")
+
+
+print_hanging_indents(rosetti_unformatted)
