b74b4ca92473691c4292353eefc9f9fb8077574e
Peter Ziersch <peter.ziersch@ecocentric.energy>
2024-04-21 17:41:59 +0800

    more fixes

--- 71/record.py
+++ 71/record.py
@@ -2,11 +2,13 @@ class RecordScore():
     """Class to track a game's maximum score"""
 
     def __init__(self):
-        self.score = 0
+        self.score = None
 
     def __call__(self, *args, **kwargs):
         """ make sure there is at least one are to use"""
         if len(args) > 0:
+            if self.score is None:
+                self.score = args[0]
             self.score = max(args[0], self.score)
 
         return self.score
