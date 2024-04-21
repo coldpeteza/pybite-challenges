9019edff41b7a69a9f4cd067604c3de586478531
Peter Ziersch <peter.ziersch@ecocentric.energy>
2024-04-21 17:38:31 +0800

    pybites challenge 71 fix

--- 71/record.py
+++ 71/record.py
@@ -2,12 +2,14 @@ class RecordScore():
     """Class to track a game's maximum score"""
 
     def __init__(self):
-        self.score = None
+        self.score = 0
 
     def __call__(self, *args, **kwargs):
         """ make sure there is at least one are to use"""
-        if self.score is not None and len(args) > 0:
+        if len(args) > 0:
             self.score = max(args[0], self.score)
+
+        return self.score
 
 
 
