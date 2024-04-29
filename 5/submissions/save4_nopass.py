1f49bef609ef91a9d72da64def9c68644260508d
Peter Ziersch <peter.ziersch@ecocentric.energy>
2024-04-30 07:08:48 +0800

    pybites challenge 5

---
+++ 5/submissions/save4_nopass.py
@@ -0,0 +1,32 @@
+NAMES = ['arnold schwarzenegger', 'alec baldwin', 'bob belderbos',
+         'julian sequeira', 'sandra bullock', 'keanu reeves',
+         'julbob pybites', 'bob belderbos', 'julian sequeira',
+         'al pacino', 'brad pitt', 'matt damon', 'brad pitt']
+
+
+def dedup_and_title_case_names(names):
+    """Should return a list of title cased names,
+       each name appears only once"""
+    return [name.title() for name in set(names)]
+
+
+
+def sort_by_surname_desc(names):
+    """Returns names list sorted desc by surname"""
+    names = dedup_and_title_case_names(names)
+    return sorted(names, key=lambda x: x.split()[-1], reverse=True)
+
+
+def shortest_first_name(names):
+    """Returns the shortest first name (str).
+       You can assume there is only one shortest name.
+    """
+    names = dedup_and_title_case_names(names)
+    current_tally = 200
+    index = 0
+    for key, name in enumerate(names):
+        if len(name.split()[0]) <= current_tally:
+            current_tally = len(name.split()[0])
+            index = key
+
+    return names[index]
